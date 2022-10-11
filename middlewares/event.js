3853ffa4d844ddfe3d13b32c176ef47e7c2b9817
Serquand <esteban.vincent@efrei.net>
2022-10-11 09:00:52 +0200

    Ended

--- middlewares/event.js
+++ middlewares/event.js
@@ -1,5 +1,10 @@
-const allEvents = new Array(0)
 const uuid = require("uuid")
+
+let allEvents = [
+    { name: "Test - 1", author: "Serquand", description: "This is a test !", id: uuid.v4() }, 
+    {  name: "Test - 2", author: "Esteban", description: "This is a second test !", id: uuid.v4() }
+]
+
 
 const postEvent = (req, res) => {
     req.body.id = uuid.v4()
@@ -9,10 +14,17 @@ const postEvent = (req, res) => {
 }
 
 const deleteEvent = (req, res) => {
-    allEvents.filter(event => event.id != req.params.idEvent)
+    let temp = allEvents
+    allEvents = []
+
+    for(let i = 0; i < temp.length; i++) {
+        if(temp[i].id != req.params.idEvent) allEvents.push(temp[i]) 
+    }
     res.status(201).json(allEvents)
 }
 
-const getEvents = (req, res) => res.status(201).json(allEvents)
+const getEvents = (req, res) => {
+    res.status(201).json(allEvents)
+} 
 
 module.exports = { postEvent, getEvents, deleteEvent }
--- public/main.js
+++ public/main.js
@@ -3,12 +3,9 @@
     const toDo = document.querySelector(".todo-container")
     const formField = document.querySelectorAll(".form-field input")  
     
-    
+    document.querySelector(".add-event").addEventListener("click", () => document.querySelector(".modal").style.display = 'flex')
 
-    document.querySelector("button").addEventListener("click", async () => {
-        console.log("We have clicked on it !")
-        console.log(formField[0]);
-        console.log(formField[0].value, formField[1].value, formField[2].value)
+    document.querySelector("button.submit-button").addEventListener("click", async () => {
         const requestOptions = {
             method: "POST", 
             headers: { "Content-Type": "application/json" },
@@ -20,5 +17,30 @@
         }
 
         allEvents = await (await fetch("http://localhost:5000/createEvent", requestOptions)).json()
+        displayEvents()
+        document.querySelector(".modal").style.display = 'none'
     })
+
+    const displayEvents = () => {
+        toDo.innerHTML = ''
+        allEvents.forEach(event => {
+            toDo.innerHTML += `
+                <div id="${event.id}" class="event">
+                    <p><span>Name: </span>${event.name}</p>
+                    <p><span>Author : </span>${event.author}</p>
+                    <p><span>Description : </span>${event.description}</p>
+                    <button class="delete-event">Delete</button>
+                </div>
+            `
+        })   
+
+        deleteEvent = async (el) => {
+            allEvents = await (await fetch("http://localhost:5000/" + el.parentNode.id, { method: 'DELETE' })).json();
+            displayEvents()
+        }
+
+        document.querySelectorAll(".delete-event").forEach(el => el.addEventListener("click", () => deleteEvent(el)))   
+    }
+
+    displayEvents()
 })();
